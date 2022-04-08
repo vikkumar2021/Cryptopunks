20450d78707be0cd1f2451a485a45d5f2390d851
tommyhabibe <tthabibe@habibe.com>
2022-04-07 18:34:17 -0700

    Create testproject.py

---
+++ Strategy Evalaution/testproject.py
@@ -0,0 +1,17 @@
+"""
+Student Name: Tommy Habibe (replace with your name)  		  	   		   	 		  		  		    	 		 		   		 		  
+GT User ID: thabibe3 (replace with your User ID)  		  	   		   	 		  		  		    	 		 		   		 		  
+GT ID: 902970734 (replace with your GT ID)  		  	   		   	 		  		  		    	 		 		   		 		  
+"""  		  	   		   	 		  		  		    	 		 		   		 		  
+
+import ManualLearner
+import StrategyLearner
+import experiment1
+import experiment2
+
+if __name__ == "__main__":
+    experiment1.main()
+    experiment2.main()
+    ManualLearner.main()
+    StrategyLearner.main()
+
