30e710cfb914d0a5469801c373bdfc485ba231f7
tommyhabibe <tthabibe@habibe.com>
2022-04-11 22:38:49 -0700

    Update experiment3.py

--- Q Learner/Strategy Evalaution/experiment3.py
+++ Q Learner/Strategy Evalaution/experiment3.py
@@ -8,6 +8,8 @@ import math
 from ManualLearner import ManualLearner
 from StrategyLearner import StrategyLearner
 import json
+from cryptopunks_data_pipeline import data_preparation_pipeline
+import os
   		  	   		   	 		  		  		    	 		 		   		 		  
 
 def author(self):
@@ -37,18 +39,40 @@ def df_to_order(df, symbol, sd=dt.datetime(2014, 9, 20), ed=dt.datetime(2020, 12
 
 def main():
     
+    with open("config_file.json") as f:
+
+        config = json.load(f)
+    
+    symbol = config.get("ticker")
+    training_sd = config.get("training_sd")
+    training_ed = config.get("training_ed")
+    test_sd = config.get("test_sd")
+    test_ed = config.get("test_ed")
+    sv = config.get("sv")
+    sma_window = config.get("sma_window")
+    bollinger_band_sma = config.get("bollinger_band_sma")
+    bollinger_band_stdev = config.get("bollinger_band_stdev")
+    so_window = config.get("so_window")
+    so_window_sma = config.get("so_window_sma")
+    obv = config.get("obv")
+    mom_window = config.get("mom_window")
+    alpha = config.get("alpha")
+    gamma = config.get("gamma")
+    rar = config.get("rar")
+    radr = config.get("radr")
+    
     #Training#
     pd.set_option('mode.chained_assignment', None)
 
-    symbol = 'BTC'
-    sd = dt.datetime(2018, 1, 1)
-    ed = dt.datetime(2020, 12, 30)
-    sv = 1000000
+    symbol = symbol
+    sd = training_sd
+    ed = training_ed
+    sv = sv
 
-    SLlearner = StrategyLearner(alpha=0.05,
-                 gamma=0.95,
-                 rar=0.99,
-                 radr=0.9)
+    SLlearner = StrategyLearner(alpha=alpha,
+                 gamma=gamma,
+                 rar=rar,
+                 radr=radr)
     SLlearner.add_evidence(symbol, sd, ed)
     df1 = SLlearner.testPolicy(symbol, sd, ed)
     
@@ -85,13 +109,13 @@ def main():
     #Testing#
     
     symbol = 'BTC'
-    sd = dt.datetime(2021, 1, 1)
-    ed = dt.datetime(2022, 3, 30)
-    sv = 1000000
+    sd2 = test_sd
+    ed2 = test_ed
+    sv2 = sv
 
-    df2 = SLlearner.testPolicy(symbol, sd, ed)
+    df2 = SLlearner.testPolicy(symbol, sd2, ed2)
     
-    df2 = df_to_order(df=df2, symbol=symbol, sd=sd, ed=ed)
+    df2 = df_to_order(df=df2, symbol=symbol, sd=sd2, ed=ed2)
     dfgains2 = compute_portvals(df2, start_val=sv)
 
     order = [(1000, sd, 'BUY', symbol),
